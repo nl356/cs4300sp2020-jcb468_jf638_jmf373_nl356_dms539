1f8f0b741ae70ebdb1b7fd621f09587846c87b6a
Joseph Fulgieri <jmf373@cornell.edu>
2020-04-27 17:31:04 -0400

    Storing matrix as inverted index instead of a numpy matrix

--- tfidf/tfidf.py
+++ tfidf/tfidf.py
@@ -6,6 +6,7 @@ import re
 import json
 from movies.movies import read_movies_json
 from songs.songs import read_songs_json
+from time import time
 
 def build_vectorizer(max_features, stop_words = 'english', norm='l2'):
     """Returns a TfidfVectorizer object with the above preprocessing properties.
@@ -41,7 +42,54 @@ def build_matrix():
     words_list.append(i["lyrics"])
 
   movie_and_songs_by_words = tfidf_vec.fit_transform(words_list).toarray()
+
   return movie_and_songs_by_words
+
+def build_inverted_index_from_matrix(matrix):
+  """Creates an inverted index from a tf-idf matrix
+  maps: word index (int) --> list of tuples (document index, occurences of the given word in this doc)
+  """
+
+  matrix = matrix.transpose()
+
+  index = {}
+  word_index = 0
+  doc_index = 0
+
+  for word_col in matrix:
+    index[word_index] = []
+
+    for doc_occ in word_col:
+
+      if doc_occ != 0:
+        index[word_index].append( (doc_index, doc_occ) )
+
+
+      doc_index += 1
+
+    doc_index = 0
+    word_index += 1
+
+
+  return index
+
+
+def build_matrix_from_inverted_index(index, num_movies):
+  """Recreates the tf-idf matrix from an inverted index"""
+
+  matrix = np.zeros( ( num_movies, len(index)) )
+
+  w_index = 0
+
+  for word in index:
+    for doc_tup in index[word]:
+      matrix[doc_tup[0]][w_index] = doc_tup[1]
+
+    w_index += 1
+
+  return matrix
+
+
 
 def read_tfidf_matrix():
   """
@@ -57,5 +105,37 @@ def write_matrix():
   movie_and_songs_by_words_matrix = build_matrix()
   np.save('tfidf/tfidf_matrix.npy', movie_and_songs_by_words_matrix)
 
+
+def read_index():
+  """reads inverted index"""
+
+  with open('./tfidf/index.json', 'r') as f:
+    data = json.load(f)
+
+  print('read index')
+
+  return data
+
+
+def write_index(index):
+  with open('./tfidf/index.json', 'w') as f:
+    json.dump(index, f, indent=4)
+
+    print("wrote %i entries" % len(index))
+
+
+def fetch_cached_matrix():
+  """Reads the inverted index and constructs and returns the tf-idf matrix from it"""
+
+  index = read_index()
+
+  ## IF NUMBER OF DATA POINTS CHANGES, HARD CODED VALUE MUST ALSO BE CHANGED
+  # I didn't want to read the data to find out it's length in order to save time
+  return build_matrix_from_inverted_index(index, 39406)
+
+
 if __name__ == "__main__":
-  write_matrix()
+
+  fetch_cached_matrix()
+
+
