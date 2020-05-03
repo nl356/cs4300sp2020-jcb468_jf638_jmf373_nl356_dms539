922c6ab11f0476aa0095576c4abe706fbef4ca15
nat <nl356@cornell.edu>
2020-05-02 20:04:36 -0400

    add advanced search field and starting autocomplete

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 from . import *  
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
@@ -6,14 +7,15 @@ from app.irsystem.models.search import main_search
 from songs.songs import read_songs_json
 
 song_data = read_songs_json()
-title_artist_list = sorted([(song["name"],song["artists"]) for song in song_data])
+title_artist_list = sorted([str(song["name"]+" ("+song["artists"]+")") for song in song_data])
 title_list = sorted([song["name"] for song in song_data])
 
 @irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
 	year = request.args.get('year')
-	rating = request.args.get('rating')
+	rating = request.args.get('rating') 
+	dislikeSong = request.args.get('dislikeSong')
 	if not query:
 		data = []
 		output_message = 'Please enter a song title above to see results!'
@@ -26,3 +28,4 @@ def search():
 			data = []
 			output_message = "No results for the song \"" + query + "\". Please enter another song title."
 	return render_template('search.html', output_message=output_message, data=data, song_list=title_artist_list)
+	# return render_template('search.html', output_message=output_message, data=data, song_list=json.dumps(title_artist_list))
